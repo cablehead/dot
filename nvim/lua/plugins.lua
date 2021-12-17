1212d8b89a3cd3fda1fb5f1175cdf611001ac0e7
Andy Gayton <andy@thecablelounge.com>
2021-12-17 11:22:07 -0500

    add nvim config

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,15 @@
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use 'nvim-lua/plenary.nvim'
+  use 'famiu/nvim-reload'
+
+  use 'nathom/tmux.nvim'
+
+  use {'ibhagwan/fzf-lua', requires = {'vijaymarupudi/nvim-fzf'}}
+
+  use {'bfredl/nvim-luadev'}
+  use {'lukas-reineke/format.nvim'}
+
+end)
