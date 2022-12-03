f899217503ba56761f1e6844761e5199b60589b4
Andy Gayton <andy@thecablelounge.com>
2022-12-02 23:10:21 -0500

    format

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,13 +1,19 @@
-return require('packer').startup(function()
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+return require("packer").startup(function()
+    -- Packer can manage itself
+    use("wbthomason/packer.nvim")
 
-  use 'nvim-lua/plenary.nvim'
-  use 'famiu/nvim-reload'
+    use("nvim-lua/plenary.nvim")
+    use("famiu/nvim-reload")
 
-  use 'nathom/tmux.nvim'
+    use("nathom/tmux.nvim")
 
-  use {'bfredl/nvim-luadev'}
+    use({ "bfredl/nvim-luadev" })
 
-  use {'mhartington/formatter.nvim'}
+    use({ "mhartington/formatter.nvim" })
+
+    use({
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.0",
+        requires = { { "nvim-lua/plenary.nvim" } },
+    })
 end)
