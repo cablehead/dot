e6916dab881f80fe4303bf0fcd7aae751baeb8c2
Andy Gayton <andy@thecablelounge.com>
2023-11-01 23:06:29 -0400

    nvim: small steps into LSP

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -28,28 +28,7 @@ return require("packer").startup(function()
 
     use({ "nvim-telescope/telescope-file-browser.nvim" })
 
-		--[[
-    use({
-        "VonHeikemen/lsp-zero.nvim",
-        branch = "v2.x",
-        requires = {
-            -- LSP Support
-            { "neovim/nvim-lspconfig" }, -- Required
-            { -- Optional
-                "williamboman/mason.nvim",
-                run = function()
-                    pcall(vim.cmd, "MasonUpdate")
-                end,
-            },
-            { "williamboman/mason-lspconfig.nvim" }, -- Optional
-
-            -- Autocompletion
-            { "hrsh7th/nvim-cmp" }, -- Required
-            { "hrsh7th/cmp-nvim-lsp" }, -- Required
-            { "L3MON4D3/LuaSnip" }, -- Required
-        },
-    })
-		--]]
+		use('neovim/nvim-lspconfig')
 
     -- use("github/copilot.vim")
 end)
